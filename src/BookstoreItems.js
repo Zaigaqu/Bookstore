a371f3ee59fdc17b0eccb52d412f77519356ad2c
alondo1 <alondo1@gmail.com>
2021-05-16 22:49:59 -0400

    Formatting Chanages

--- src/BookstoreItems.js
+++ src/BookstoreItems.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import BooksList from './BooksList';
+import React from "react";
+import BooksList from "./BooksList";
 //import classes from './BookstoreItems.module.css';
 
-const BookstoreItems = props => {
-    return (
-        <React.Fragment>
-            <h2>Hello World</h2>
-            <BooksList books={props.books}/>
-        </React.Fragment>
-    );
+const BookstoreItems = (props) => {
+  return (
+    <React.Fragment>
+      <h2>Hello World</h2>
+      <BooksList books={props.books} />
+    </React.Fragment>
+  );
 };
 
 export default BookstoreItems;
