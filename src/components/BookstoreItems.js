b8be6e93a677950a0a0cffb29dbabf9ea82bc30f
Zaigaqu <violetteben@gmail.com>
2021-05-23 14:16:38 -0400

    test

--- src/components/BookstoreItems.js
+++ src/components/BookstoreItems.js
@@ -1,5 +1,6 @@
 import React from "react";
 import BooksList from "./BooksList";
+import MovieList from './MovieList';
 //import classes from './BookstoreItems.module.css';
 
 const BookstoreItems = (props) => {
@@ -7,8 +8,11 @@ const BookstoreItems = (props) => {
     <React.Fragment>
       <h2>Hello World</h2>
       <BooksList books={props.books} />
+      <MovieList movies={props.movies}/>
     </React.Fragment>
   );
 };
+
+
 
 export default BookstoreItems;
